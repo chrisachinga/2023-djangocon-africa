82bee8e812704aaa1751cc1b14285be44699f0bf
doghot <achinga.chris@gmail.com>
2023-10-29 14:48:03 +0300

    created forms to create mentor and mentee account, user login form. Created test for the forms

--- server/app/tests.py
+++ server/app/tests.py
@@ -1,6 +1,8 @@
 from django.test import TestCase
 from django.core.exceptions import ValidationError
 from django.utils import timezone
+from django.contrib.auth import get_user_model
+from app.forms import MentorSignUpForm, MenteeSignUpForm, UserLoginForm
 from app.models import CustomUser, Skill, WorkExperience, Education, MentorProfile, MenteeProfile
 
 class CustomUserTestCase(TestCase):
@@ -78,3 +80,62 @@ class ProfileTestCase(TestCase):
         mentee_profile.delete()
         with self.assertRaises(MenteeProfile.DoesNotExist):
             MenteeProfile.objects.get(user=self.user_mentee)
+
+
+# Testing Forms
+class MentorSignUpFormTest(TestCase):
+
+    def test_mentor_signup_form_valid_data(self):
+        form = MentorSignUpForm(data={
+            'username': 'mentorusername',
+            'password1': 'testpassword123',
+            'password2': 'testpassword123',
+            'gender': 'Male',
+            'year_of_birth': 1980,
+            'bio': 'I am a mentor.'
+        })
+        self.assertTrue(form.is_valid())
+
+    def test_mentor_signup_form_no_data(self):
+        form = MentorSignUpForm(data={})
+        self.assertFalse(form.is_valid())
+        self.assertEqual(len(form.errors), 4)
+
+
+class MenteeSignUpFormTest(TestCase):
+
+    def test_mentee_signup_form_valid_data(self):
+        form = MenteeSignUpForm(data={
+            'username': 'menteeusername',
+            'password1': 'testpassword123',
+            'password2': 'testpassword123',
+            'gender': 'Female',
+            'year_of_birth': 1990,
+            'bio': 'I am a mentee.'
+        })
+        self.assertTrue(form.is_valid())
+
+    def test_mentee_signup_form_no_data(self):
+        form = MenteeSignUpForm(data={})
+        self.assertFalse(form.is_valid())
+        self.assertEqual(len(form.errors), 4)
+
+class UserLoginFormTest(TestCase):
+
+    def setUp(self):
+        self.user = get_user_model().objects.create_user(
+            username='testuser',
+            password='testpass123'
+        )
+
+    def test_user_login_form_valid_data(self):
+        form = UserLoginForm(data={
+            'username': 'testuser',
+            'password': 'testpass123',
+        })
+        self.assertTrue(form.is_valid())
+
+    def test_user_login_form_no_data(self):
+        form = UserLoginForm(data={})
+        self.assertFalse(form.is_valid())
+        self.assertEqual(len(form.errors), 2)  # username and password fields are required
