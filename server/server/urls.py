5bcbd0762804823889a015b17986c609f72dc220
doghot <achinga.chris@gmail.com>
2023-10-29 11:54:13 +0300

    sign up form

--- server/server/urls.py
+++ server/server/urls.py
@@ -20,6 +20,7 @@ from django.views.generic.base import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("app/", include("app.urls")),
     path("accounts/", include("django.contrib.auth.urls")),
     path("", TemplateView.as_view(template_name="home.html"), name="home"),
 ]
